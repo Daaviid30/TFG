b0d1777475cb59abf2863fc91569b339f5dbffb9
Daaviid30 <davidmartinc30@gmail.com>
2025-03-13 13:04:03 +0100

    color graph changes

--- webGraph.py
+++ webGraph.py
@@ -18,24 +18,24 @@ def create_graph(report: dict):
     for node in report:
         type = node["nodeType"]
         if type == "apiCall":
-            graph.add_node(node["apiCall"])
+            graph.add_node(node["apiCall"], color = "#FF002E")
         elif type == "domElement":
-            graph.add_node(node["elementID"])
+            graph.add_node(node["elementID"], color = "#FFC500")
         elif type == "eventListener":
-            graph.add_node(node["type"])
+            graph.add_node(node["type"], color = "#CDFF00")
         elif type == "executionContext":
-            graph.add_node(node["executionContextID"])
+            graph.add_node(node["executionContextID"], color = "#00FF00")
         elif type == "extension":
-            graph.add_node(node["extensionID"])
+            graph.add_node(node["extensionID"], color = "#0000FF")
         elif type == "network":
-            graph.add_node(node["requestID"])
+            graph.add_node(node["requestID"], color = "#00FFBD")
         elif type == "page":
-            graph.add_node(node["pageID"])
+            graph.add_node(node["pageID"], color = "#00D1FF")
         elif type == "script":
-            graph.add_node(node["scriptID"])
+            graph.add_node(node["scriptID"], color = "#FF008F")
         elif type == "target":
             if node["event"] == "create":
-                graph.add_node(node["targetID"])
+                graph.add_node(node["targetID"], color = "#909090")
 
     # Export the graph to a gexf file (for Gephi visualitation)
     nx.write_gexf(graph, "webGraph.gexf")
